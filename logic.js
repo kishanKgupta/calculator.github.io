6e00c8b417bf3f439b8ae760ce8db47b5614e98b
kishanKgupta <kkgupta0106@gmail.com>
2023-11-03 17:24:54 +0530

    COMMIT

---
+++ logic.js
@@ -0,0 +1,32 @@
+//let num1=0, num2=0, num=0;
+document.querySelector('.input').value = 0;
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=')
+        {
+            string = eval(string);
+             document.querySelector('.input').value = string;
+        }
+        else if (e.target.innerHTML == 'C')
+        {
+            string = "";
+            document.querySelector('.input').value = string;
+            document.querySelector('.input').value = 0;        
+        }
+        else
+        {
+            console.log(e.target);
+        string = string + e.target.innerHTML;
+        document.querySelector('.input').value = string;
+            }
+        
+    })
+})
+
+
+
+
+
+
